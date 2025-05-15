28e8ee4d7e0e4e1e5193f71b57202c117f3b8f1d
Javier-Espana <javier.eduardo.espana@gmail.com>
2025-05-14 18:37:58 -0600

    Fixed fix

---
+++ src/components/Login.vue
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '@/views/LoginView.vue'
+import RegisterView from '@/views/RegisterView.vue'
+import DashboardView from '@/views/DashboardView.vue'
+import CalendarView from '@/views/CalendarView.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView,
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: RegisterView,
+  },
+  {
+    path: '/dashboard',
+    mame: 'dashboard',
+    component: DashboardView,
+  },
+  {
+    path: '/calendar',
+    name: 'calendar',
+    component: CalendarView,
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
