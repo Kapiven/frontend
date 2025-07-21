63756ed371412456fd8592b63cd3221763cc5b54
TonitoMC <joseameridac@gmail.com>
2025-07-20 20:31:01 -0600

    temp: workflow test

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// Comment for workflow
+// Comment for Workflow V2
 const app = createApp(App)
 
 app.use(createPinia())
