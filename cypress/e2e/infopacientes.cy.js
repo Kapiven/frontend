8ad5ebc62190f201f390669efa9cab65e06b470b
TonitoMC <joseameridac@gmail.com>
2025-08-24 22:03:11 -0600

    feat: cypress e2e tests

---
+++ cypress/e2e/infopacientes.cy.js
@@ -0,0 +1,16 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('http://localhost:8080/register')
+
+    cy.get('a').click()
+    cy.get('#username').click()
+    cy.get('#username').clear()
+    cy.get('#username').type('tono')
+    cy.get('#password').click()
+    cy.get('#password').clear()
+    cy.get('#password').type('E')
+    cy.get('#password').clear()
+    cy.get('#password').type('Contrasena1!')
+    cy.get('.auth-button').click()
+  })
+})
