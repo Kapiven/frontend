8effbac99f2057f7444bbe6083727d77a0f34b32
TonitoMC <joseameridac@gmail.com>
2025-10-23 19:21:32 -0600

    update frontend prod docker

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://18.219.58.209:4000'
+const API_BASE_URL = import.meta.env.VITE_BACKEND_URL || 'http://backend:4000'
 
 // DEBUG - remove after we figure this out
 console.log('VITE_BACKEND_URL:', import.meta.env.VITE_BACKEND_URL)
